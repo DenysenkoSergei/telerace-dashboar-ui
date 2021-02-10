4ada90f43b559073beb899e4f68ef8a13fcb0b8c
sergii <denysenkosergei@gmail.com>
2021-02-10 12:02:17 +0200

    proxy fixes

--- src/js/Config.js
+++ src/js/Config.js
@@ -3,7 +3,7 @@ class ConfigUtils {
     static apiUrl = 'http://95.84.140.16:81/api'
 
     static prepareRequestUrl(path) {
-        return apiUrl + path;
+        return this.apiUrl + path;
     }
 
 }
