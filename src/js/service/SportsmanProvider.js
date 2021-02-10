705fe3b670e8fa2ce8f98e9424209f99c27beff7
sergii <denysenkosergei@gmail.com>
2021-02-10 13:56:12 +0200

    proxy fixes

--- src/js/service/SportsmanProvider.js
+++ src/js/service/SportsmanProvider.js
@@ -5,16 +5,7 @@ class SportsmanProvider {
 
     static loadSportsmen(callback) {
         axios.get(
-            ConfigUtils.prepareRequestUrl("/athlete"),
-            {
-                headers: {
-                    "Access-Control-Allow-Credentials": "true",
-                    "Access-Control-Allow-Origin": "*",
-                    "Access-Control-Allow-Methods": "GET,OPTIONS,PATCH,DELETE,POST,PUT",
-                    "Access-Control-Allow-Headers": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-                }
-            }
-        )
+            ConfigUtils.prepareRequestUrl("/athlete"))
             .then(callback)
     }
 
