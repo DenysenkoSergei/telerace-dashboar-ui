6179c2907de6d649df4211dddf0643bd5848657d
sergii <denysenkosergei@gmail.com>
2021-02-16 12:09:29 +0200

    Pop-up sportsman details

--- src/js/Config.js
+++ src/js/Config.js
@@ -1,6 +1,6 @@
 class ConfigUtils {
 
-    static apiUrl = 'http://95.84.140.16:81/api'
+    static apiUrl = 'https://hub.telerace.ru/api'
 
     static prepareRequestUrl(path) {
         return this.apiUrl + path;
